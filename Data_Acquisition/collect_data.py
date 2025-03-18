bc545e9556a61db7b27c129f48ddc37feb3af1b4
Manar212-zjqhj <manar.benabdallah.2000@gmail.com>
2025-03-18 02:25:33 +0100

    Update collect_data.py

--- Data_Acquisition/collect_data.py
+++ Data_Acquisition/collect_data.py
@@ -1,1 +1,20 @@
+import serial
+import csv
+import time
 
+ser = serial.Serial("COM3", 115200)
+time.sleep(2)
+
+with open("vibrations.csv", "w", newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["X", "Y", "Z", "Label"])
+
+    print("Collecte en cours... Appuyez sur Ctrl+C pour arrêter !")
+    
+    try:
+        while True:
+            ligne = ser.readline().decode('utf-8').strip()
+            writer.writerow(ligne.split(","))
+    except KeyboardInterrupt:
+        print("Arrêt de la collecte.")
+        ser.close()
