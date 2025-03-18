b6b4e52edce9ec7bd282e9bf823df631979e9831
Manar212-zjqhj <manar.benabdallah.2000@gmail.com>
2025-03-18 02:26:03 +0100

    Update vibration_sensor.ino

--- Data_Acquisition/vibration_sensor.ino
+++ Data_Acquisition/vibration_sensor.ino
@@ -1,1 +1,28 @@
+#include <Arduino_LSM9DS1.h>
 
+void setup() {
+    Serial.begin(115200);
+    while (!Serial);
+    
+    if (!IMU.begin()) {
+        Serial.println("Erreur : Impossible d'initialiser l'IMU !");
+        while (1);
+    }
+    Serial.println("IMU Prêt !");
+}
+
+void loop() {
+    float x, y, z;
+    
+    if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(x, y, z);
+        Serial.print(x);
+        Serial.print(",");
+        Serial.print(y);
+        Serial.print(",");
+        Serial.print(z);
+        Serial.print(",1");  // Modifier l'étiquette selon la vibration
+        Serial.println();
+    }
+    delay(100);
+}
